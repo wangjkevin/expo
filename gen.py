0439f66a9fe3d13aa0c60d749cc284d97afe94f6
unknown <kevinwangqi2@gmail.com>
2026-07-10 21:02:19 -0700

    fix: format generated html file correctly

--- gen.py
+++ gen.py
@@ -20,20 +20,18 @@ def main():
 
     with open(args.html_file, "w") as f:
         f.write(
-            f"""
-            <span id="renderer" data-md="{args.markdown_file}" data-css="{args.css_file}"></span>
-
-            <!-- LaTeX typesetting support! -->
-            <script id="MathJax-script" src="https://cdn.jsdelivr.net/npm/mathjax@4/tex-mml-chtml.js"></script>
-
-            <!-- for awesome code highlighting :-) -->
-            <script src="https://cdn.jsdelivr.net/npm/prismjs@1.29.0/components/prism-core.min.js"></script>
-            <script src="https://cdn.jsdelivr.net/npm/prismjs@1.29.0/components/prism-clike.min.js"></script>
-            <script src="https://cdn.jsdelivr.net/npm/prismjs@1.29.0/components/prism-c.min.js"></script>
-            <script src="https://cdn.jsdelivr.net/npm/prismjs@1.29.0/components/prism-cpp.min.js"></script>
-
-            <script type="module" src="expo.js"></script>
-            """
+            f"<span id='renderer' data-md='{args.markdown_file}' data-css='{args.css_file}'></span>\n"
+            f"\n"
+            f"<!-- LaTeX typesetting support! -->\n"
+            f"<script id='MathJax-script' src='https://cdn.jsdelivr.net/npm/mathjax@4/tex-mml-chtml.js'></script>\n"
+            f"\n"
+            f"<!-- for awesome code highlighting :-) -->\n"
+            f"<script src='https://cdn.jsdelivr.net/npm/prismjs@1.29.0/components/prism-core.min.js'></script>\n"
+            f"<script src='https://cdn.jsdelivr.net/npm/prismjs@1.29.0/components/prism-clike.min.js'></script>\n"
+            f"<script src='https://cdn.jsdelivr.net/npm/prismjs@1.29.0/components/prism-c.min.js'></script>\n"
+            f"<script src='https://cdn.jsdelivr.net/npm/prismjs@1.29.0/components/prism-cpp.min.js'></script>\n"
+            f"\n"
+            f"<script type='module' src='expo.js'></script>\n"
         )
 
 if __name__ == "__main__":
