0275601a14ce077d468703f1ac88e16158cef9ed
Kevin J Wang <kevinwangqi2@gmail.com>
2026-07-11 18:11:57 -0700

    fix: bypass cors again again

--- gen.py
+++ gen.py
@@ -19,6 +19,16 @@ def parse_args():
 def main():
     args = parse_args()
 
+    # using realpath instead of abspath method resolves symlinks, which is needed on 
+    # myth, as ".ir" points to ".ir.stanford.edu"
+    dir_of_this_script = os.path.dirname(os.path.realpath(__file__))
+
+    path_of_expo_entrypoint = os.path.join(dir_of_this_script, "expo.js")
+
+    dir_of_html_file = os.path.dirname(os.path.realpath(args.html_file))
+
+    relative_path_to_expo_entrypoint = os.path.relpath(path_of_expo_entrypoint, start=dir_of_html_file)
+
     with open(args.html_file, "w") as f:
         f.write(
             f"<span id='renderer' data-md='{args.markdown_file}' data-css='{args.css_file}'></span>\n"
@@ -33,7 +43,7 @@ def main():
             f"<script src='https://cdn.jsdelivr.net/npm/prismjs@1.29.0/components/prism-cpp.min.js'></script>\n"
             f"\n"
             # thank you, [[ http://stackoverflow.com/questions/5137497/find-the-current-directory-and-files-directory ]] !
-            f"<script type='module' src={os.path.dirname(os.path.abspath(__file__))}></script>\n"
+            f"<script type='module' src={relative_path_to_expo_entrypoint}></script>\n"
         )
 
 if __name__ == "__main__":
